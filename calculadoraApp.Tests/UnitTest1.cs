7f44d20b0dda0d8598980a5287fe89fb6c2faf39
fabia <fabianmediavilla27@gmail.com>
2025-12-09 11:04:14 -0500

    Proyecto Calculadora

---
+++ calculadoraApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿
+namespace calculadoraApp.Tests
+{
+    public class CalculadoraAppTests
+    {
+        [Fact]
+        public void Sumar_DosNumeros_VerificaSumaCorrecta()
+        {
+            // Arrange
+            var calc = new CalculadoraApp.calculadoraApp();
+            var resultado = calc.Sumar(2, 3);
+            Assert.Equal(5, resultado);
+        }
+    }
+}
---
+++ calculadoraApp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+namespace CalculadoraApp
+{
+    public class calculadoraApp
+    {
+        public int Sumar(int valor1, int valor2) => valor1 + valor2;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var calc= new calculadoraApp();
+           Console.WriteLine($"2+3 {calc.Sumar(2, 3)}");
+        }
+    }
+
+}
